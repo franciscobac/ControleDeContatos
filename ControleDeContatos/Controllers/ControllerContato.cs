d5c6a38cc162318297959fd957d3173f0a610d53
franciscobac <franciscobac@hotmail.com>
2023-07-31 04:06:00 -0300

    Ajustando problema com o botão deletar

--- ControleDeContatos/Controllers/ControllerContato.cs
+++ ControleDeContatos/Controllers/ControllerContato.cs
@@ -24,14 +24,22 @@ namespace ControleDeContatos.Controllers
             return View();
         }
 
-        public IActionResult Editar()
+        public IActionResult Editar(int id)
         {
-            return View();
+            ContatoModel contato = _contatoRepositorio.ListarPorId(id);
+            return View(contato);
         }
 
-        public IActionResult ApagarConfirmacao()
+        public IActionResult ApagarConfirmacao(int id)
         {
-            return View();
+            ContatoModel contato = _contatoRepositorio.ListarPorId(id);
+            return View(contato);
+        }
+
+        public IActionResult Apagar(int id)
+        {
+            _contatoRepositorio.Apagar(id);
+            return RedirectToAction("Index");
         }
 
         [HttpPost]
@@ -40,6 +48,13 @@ namespace ControleDeContatos.Controllers
             _contatoRepositorio.Adicionar(contato);
             return RedirectToAction("Index");
         }
-        
+
+        [HttpPost]
+        public IActionResult Alterar(ContatoModel contato)
+        {
+            _contatoRepositorio.Atualizar(contato);
+            return RedirectToAction("Index");
+        }
+
     }
 }
--- ControleDeContatos/Repositorio/ContatoRepositorio.cs
+++ ControleDeContatos/Repositorio/ContatoRepositorio.cs
@@ -13,16 +13,53 @@ namespace ControleDeContatos.Repositorio
             _bancoContext = bancoContext;
         }
 
+        public ContatoModel ListarPorId(int id)
+        {
+            var retornoListarPorId = _bancoContext.Contatos.FirstOrDefault(x => x.Id == id);
+            if (retornoListarPorId == null) throw new Exception("Houve um erro na listagem de contatos!");
+
+            return retornoListarPorId;
+        }
+
         public List<ContatoModel> BuscarTodos()
         {
             return _bancoContext.Contatos.ToList();
 
         }
+
         public ContatoModel Adicionar(ContatoModel contato)
         {
             _bancoContext.Contatos.Add(contato);
             _bancoContext.SaveChanges();
             return contato;
+        }
+
+        public ContatoModel Atualizar(ContatoModel contato)
+        {
+            ContatoModel contatoDB = ListarPorId(contato.Id);
+
+            if (contatoDB == null) throw new Exception("Houve um erro na atualização do contato");
+
+            contatoDB.Nome = contato.Nome;
+            contatoDB.Email = contato.Email;
+            contatoDB.Celular = contato.Celular;
+
+            _bancoContext.Contatos.Update(contatoDB);
+            _bancoContext.SaveChanges();
+
+            return contatoDB;
+        }
+
+        public ContatoModel Apagar(int id)
+        {
+            ContatoModel contatoDB = ListarPorId(id);
+
+            if (contatoDB == null) throw new Exception("Houve um erro na deleção do contato");
+
+            _bancoContext.Contatos.Remove(contatoDB);
+            _bancoContext.SaveChanges();
+
+            return contatoDB;
         }
     }
 }
